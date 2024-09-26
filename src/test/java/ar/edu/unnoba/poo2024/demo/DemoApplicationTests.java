aba0946dddc5bea75fe34198d38701bf7ebf2d33
Jeremias Aguirres <jaguirres@comunidad.unnoba.edu.ar>
2024-09-26 15:53:11 -0300

    first commit

---
+++ src/test/java/ar/edu/unnoba/poo2024/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.poo2024.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
