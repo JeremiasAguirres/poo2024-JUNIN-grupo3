f810dfb3c96f2e348b8dbd0e242839e616dcd8bd
JoaquinHubner <joaquinhubner42@gmail.com>
2024-10-20 20:23:46 -0300

    Added Playlist class to model

--- src/main/java/ar/edu/unnoba/poo2024/allmusic/model/Playlist.java
+++ src/main/java/ar/edu/unnoba/poo2024/allmusic/model/Playlist.java
@@ -1,4 +1,31 @@
 package ar.edu.unnoba.poo2024.allmusic.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="playlist")
 public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String namePlaylist;
+
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.namePlaylist;
+    }
+
+    public void setName(String name) {
+        this.namePlaylist = name;
+    }
+
 }
