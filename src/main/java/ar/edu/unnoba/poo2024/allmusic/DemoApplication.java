4db50b912a31b818b2bf00d491d98bd967fb3828
Jeremias Aguirres <jaguirres@comunidad.unnoba.edu.ar>
2024-10-20 17:06:01 -0300

    Setting up app name and model layer

--- src/main/java/ar/edu/unnoba/poo2024/allmusic/DemoApplication.java
+++ src/main/java/ar/edu/unnoba/poo2024/allmusic/DemoApplication.java
@@ -1,4 +1,4 @@
-package ar.edu.unnoba.poo2024.demo;
+package ar.edu.unnoba.poo2024.allmusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
