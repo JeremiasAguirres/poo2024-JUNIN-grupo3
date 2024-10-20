7187a144c5f343a2da10c57350c31ca3f058fa8a
Jeremias Aguirres <jaguirres@comunidad.unnoba.edu.ar>
2024-10-20 17:28:45 -0300

    Added class User and its subclasses

---
+++ src/main/java/ar/edu/unnoba/poo2024/allmusic/model/MusicArtistUser.java
@@ -0,0 +1,20 @@
+package ar.edu.unnoba.poo2024.allmusic.model;
+
+public class MusicArtistUser extends User{
+
+    private String artisticName;
+    
+    public boolean canCreateSongs(){
+            return true;
+        }
+    
+
+    public String getArtisticName() {
+        return this.artisticName;
+    }
+
+    public void setArtisticName(String artisticName) {
+        this.artisticName = artisticName;
+    }    
+
+}
---
+++ src/main/java/ar/edu/unnoba/poo2024/allmusic/model/MusicEnthusiastUser.java
@@ -0,0 +1,10 @@
+package ar.edu.unnoba.poo2024.allmusic.model;
+
+public class MusicEnthusiastUser {
+
+    public boolean canCreateSongs(){
+        return false;
+    }
+
+
+}
