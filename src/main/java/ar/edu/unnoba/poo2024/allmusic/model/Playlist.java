aec503cb0fc6359c791aca69ae8e6d1c679a244a
JoaquinHubner <joaquinhubner42@gmail.com>
2024-10-21 14:40:46 -0300

    Updated playlist class

--- src/main/java/ar/edu/unnoba/poo2024/allmusic/model/Playlist.java
+++ src/main/java/ar/edu/unnoba/poo2024/allmusic/model/Playlist.java
@@ -2,6 +2,8 @@ package ar.edu.unnoba.poo2024.allmusic.model;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name="playlist")
 public class Playlist {
@@ -11,6 +13,27 @@ public class Playlist {
     private long id;
     private String namePlaylist;
 
+    @ManyToOne
+    private User owner;
+
+    @ManyToMany
+    private List<Song> songs;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public List<Song> getSongs() {
+        return songs;
+    }
+
+    public void setSongs(List<Song> songs) {
+        this.songs = songs;
+    }
 
     public long getId() {
         return this.id;
