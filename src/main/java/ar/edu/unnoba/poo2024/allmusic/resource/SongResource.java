03b1ab315b7a7024db778d69239deb3ef76629cf
Jere Aguirres <jaguirres@comunidad.unnoba.edu.ar>
2024-11-20 09:38:41 -0300

    Implemented getSong method that returns a song given an id

--- src/main/java/ar/edu/unnoba/poo2024/allmusic/resource/SongResource.java
+++ src/main/java/ar/edu/unnoba/poo2024/allmusic/resource/SongResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,45 @@ public class SongResource {
     
     @GetMapping(produces = "application/json")
     public ResponseEntity<?> getSongs(@RequestHeader("Authorization") String token){
+        try{
+            authorizationService.authorize(token);
+            ModelMapper modelMapper = new ModelMapper();
+
+            modelMapper.createTypeMap(Song.class, SongResponseDTO.class)
+            .addMapping(src -> src.getAuthor().getUsername(),(dto, v) -> dto.getArtist().setName((String)v))
+            .addMapping(src -> src.getAuthor().getId(),(dto, v) -> dto.getArtist().setId((Long)v));
+
+            List<Song> songs = songService.getAll();
+            List<SongResponseDTO> dtos = songs.stream()
+            .map(song -> modelMapper.map(song, SongResponseDTO.class))
+            .collect(Collectors.toList());
+            return new ResponseEntity<>(dtos, HttpStatus.OK);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @GetMapping(value = "/{songId}",produces = "application/json")
+    public ResponseEntity<?> getSong(@RequestHeader("Authorization") String token, @PathVariable Long songId){
+        try{
+            authorizationService.authorize(token);
+            ModelMapper modelMapper = new ModelMapper();
+
+            modelMapper.createTypeMap(Song.class, SongResponseDTO.class)
+            .addMapping(src -> src.getAuthor().getUsername(),(dto, v) -> dto.getArtist().setName((String)v))
+            .addMapping(src -> src.getAuthor().getId(),(dto, v) -> dto.getArtist().setId((Long)v));
+
+            SongResponseDTO dto = modelMapper.map(songService.getSongById(songId), SongResponseDTO.class);
+            return new ResponseEntity<>(dto, HttpStatus.OK);
+        }
+        catch(Exception e){
+            return new ResponseEntity<>(null,HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @GetMapping(value = "?artist=:artist_name&genre=:genre", produces = "application/json")
+    public ResponseEntity<?> filterSongs(@RequestHeader("Authorization") String token){
         try{
             authorizationService.authorize(token);
             ModelMapper modelMapper = new ModelMapper();
--- src/main/java/ar/edu/unnoba/poo2024/allmusic/service/SongServiceImp.java
+++ src/main/java/ar/edu/unnoba/poo2024/allmusic/service/SongServiceImp.java
@@ -1,10 +1,12 @@
 package ar.edu.unnoba.poo2024.allmusic.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import ar.edu.unnoba.poo2024.allmusic.model.Genre;
 import ar.edu.unnoba.poo2024.allmusic.model.Song;
 import ar.edu.unnoba.poo2024.allmusic.repository.SongRepository;
 
@@ -19,6 +21,14 @@ public class SongServiceImp implements SongService{
         return repository.findAll();
     }
 
-    
+    @Override
+    public Optional<Song> getSongById(Long id){
+        return repository.findById(id);
+    }
+
+    @Override
+    public List<Song> filterByArtistOrGenre(String artisticName, Genre genre) {
+        return repository.findAll();
+    }
 
 }
